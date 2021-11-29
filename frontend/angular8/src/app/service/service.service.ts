01f325bcdd68b1f7a2028fdae5387d4a94beaa45
Andrey <guruspirit@mail.ru>
2021-11-29 19:36:57 +0300

    Error: Could not find or load main class com.example.sklad.Main

--- frontend/angular8/src/app/service/service.service.ts
+++ frontend/angular8/src/app/service/service.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  Url= 'http://localhost:'
 }
